3430671840889d0971022f9973b821b3593b3d7e
H-Mehmood <hassanig567@gmail.com>
2025-03-21 23:14:49 +0500

    import one file another file and added firestore

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import firebaseApp from "./firebase.js";
+import {getFirestore} from "firebase/firestore";
+
+const DB = getFirestore(firebaseApp);
 
 console.log('Firebase Connection');
 console.log(firebaseApp);
